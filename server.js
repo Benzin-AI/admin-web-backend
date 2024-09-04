a85c2cd6f3046b40cff7ac214c58eec4ca4fac6f
Sergio Montenegro <benzin321@hotmail.com>
2024-09-04 15:00:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-// server.js
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Servidor en puerto ${PORT}`);
 });
+
+// Función para manejar el cierre del servidor
+function handleShutdown(signal) {
+  console.log(`Recibida señal de ${signal}. Cerrando servidor...`);
+  server.close(() => {
+    console.log('Servidor cerrado.');
+    process.exit(0); // Salir del proceso con código 0
+  });
+}
+
+// Manejo de señales de terminación
+process.on('SIGTERM', () => handleShutdown('SIGTERM'));
+process.on('SIGINT', () => handleShutdown('SIGINT'));
